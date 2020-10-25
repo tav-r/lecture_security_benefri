afec5ae4e24e428b216cf3776dc02781c7838866
marc392 <marc.kaeser@gmail.com>
2020-10-25 09:57:54 -0700

    Create initial connection to server, sync strategy

--- ex03/tests.py
+++ ex03/tests.py
@@ -1,3 +1,6 @@
 from ldap3 import Server, Connection, ALL
 
-    server = Server('')
+server = Server('ldap.secuis.fun:8443', use_ssl=True, get_info=ALL)
+conn = Connection(server, 'cn=admin,dc=ldap,dc=secuis,dc=fun', 'ta643upKzcANJU2c!6aj', auto_bind=True)
+print(conn)
+
